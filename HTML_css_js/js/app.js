74518ef29df750f9f6be29493b5f10ea0bb3ef62
soncom <73628071+son-su-hyeong@users.noreply.github.com>
2022-07-06 10:01:33 +0900

    Add files via upload
    riew : html, css, js standard

---
+++ HTML_css_js/js/app.js
@@ -0,0 +1,30 @@
+// let random = Math.random();
+// if(random < 0.5) {
+//     console.log("YOUR NUMBER IS LESS THAN 0.5!!!")
+//     console.log(random);
+// }
+
+// if(random >= 0.5) {
+//     console.log("YOUR NUMBER IS GREATE THAN 0.5!!!")
+//     console.log(random);
+// }
+
+// const dayOfweek = 'Monday';
+
+// if(dayOfweek === 'Monday') {
+//     console.log("UGHHH I HATE MONDAYS!")
+// }
+// else if (dayOfweek === "Saturday") {
+//     console.log("YAY I LOVE SATURDAYS")
+
+// }
+
+const password = prompt("please enter a new password");
+
+if(password.length >= 6) {
+    console.log("LONG ENOUGH PASSWORD!")
+} else {
+    console.log("PASSWORD TOO SHORT! Muse be 6+ characters")
+}
+
+if(password.inb)
